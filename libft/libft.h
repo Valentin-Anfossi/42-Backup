0a2ab58394818ccdbd4c864af428bcedf874c266
vanfossi <valentin.anfossi@gmail.com>
2024-11-07 14:32:43 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -46,7 +46,7 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size);
 size_t ft_strlen(const char *s);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int ft_strncmp(const char *s1, const char *s2,  size_t n);
-char *ft_strnstr(char *big, char *little, int len);
+char *ft_strnstr(const char *big, const char *little, int len);
 char *ft_strrchr(const char *s, int c);
 char *ft_strtrim(char const *s1, char const *set);
 char *ft_substr(char const *s, unsigned int start, size_t len);
