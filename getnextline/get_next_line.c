a315cb5413fe94624df2786fc41eb19c7d59f484
vanfossi <vanfossi@DESKTOP-83L05AK.>
2024-11-16 20:06:47 +0100

    no

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -11,15 +11,52 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#define BUFFERSIZE 500
+#include <stdio.h>
 
 char *get_next_line(int fd)
 {
 	static int nline;
 	char *buffer;
+	char *line;
+	int count;
+	int lcount;
+	int i;
+	int j;
 
-	buffer = malloc(20);
-	read(fd,buffer,20);
-	return (buffer);
+	i = 0;
+	j = 0;
+	line = malloc(500);
+	buffer = malloc(BUFFERSIZE);
+	read(fd,buffer,BUFFERSIZE);
+	while(buffer[j])
+	{
+		if(buffer[j] == '\n')
+			lcount ++;
+		j ++;
+		if(lcount < nline && j == BUFFERSIZE)
+		{
+			read(fd,buffer,BUFFERSIZE);
+			continue;
+		}
+	}
+	j = 0;
+	while(buffer[j] != '\n' && buffer[j])
+	{
+		//printf("%c",buffer[j]);
+		line[i] = buffer[j];
+		j ++;
+		i ++;
+		if(j == BUFFERSIZE)
+		{
+			read(fd,buffer,BUFFERSIZE);
+			j = 0;
+			continue;
+		}
+	}
+	nline++;
+	printf("%d",nline);
+	return (line);
 }
 
 int main(void)
@@ -28,7 +65,9 @@ int main(void)
 	fd = open("./01", O_RDONLY);
 	char *result;
 
-	result = malloc(20);
+	//result = malloc(500);
 	result = get_next_line(fd);
-	printf("%s",result);
+	printf("%s\n",result);
+	result = get_next_line(fd);
+	printf("%s\n",result);
 }
