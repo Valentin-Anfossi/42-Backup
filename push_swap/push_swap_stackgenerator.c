68d8e1147496d9a967388198b251bbbf773fc584
vanfossi <vanfossi@DESKTOP-83L05AK.>
2024-11-25 04:50:40 +0100

    sah

---
+++ push_swap/push_swap_stackgenerator.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_stackgenerator.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vanfossi <vanfossi@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 11:28:03 by vanfossi          #+#    #+#             */
+/*   Updated: 2024/11/25 04:01:43 by vanfossi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define STACKSIZE 15
+
+int check_duplicate(int n, int *a);
+
+void print_stack(int *a) {
+    int i;
+
+	i = 0;
+	while(i < STACKSIZE)
+	{
+        printf("%d ", a[i]);
+		i ++;
+    }
+}
+
+int main(void) {
+    srand(time(NULL));
+    int i;
+    int r;
+    int *a;
+
+	i = 0;
+    a = malloc(sizeof(int) * STACKSIZE);
+    while (i < STACKSIZE) {
+        a[i] = -1;
+		i ++;
+    }
+	i = 0;
+    while (i < STACKSIZE) {
+        r = rand() % STACKSIZE;
+        if (!check_duplicate(r, a)) {
+            a[i] = r;
+            i++;
+        }
+    }
+    print_stack(a);
+    free(a);
+    return 0;
+}
+
+int check_duplicate(int n, int *a) {
+    int i = 0;
+    while (i < STACKSIZE) {
+        if (a[i] == n) {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
