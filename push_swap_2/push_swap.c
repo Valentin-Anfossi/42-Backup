01cf1c65382e2ee8143bc9cfb688b9ce6bc6eb00
vanfossi <vanfossi@DESKTOP-83L05AK.>
2024-11-27 19:33:37 +0100

    Added pushswap2 logic functions

--- push_swap_2/push_swap.c
+++ push_swap_2/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vanfossi <vanfossi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 07:44:30 by vanfossi          #+#    #+#             */
-/*   Updated: 2024/11/27 14:56:39 by vanfossi         ###   ########.fr       */
+/*   Updated: 2024/11/27 19:33:03 by vanfossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,7 +125,120 @@ int pa(elem **stacka, elem **stackb)
     topb->prev = NULL;
     return 1;
 }
-
+//PUSH B (PUT FIRST A ON TOP OF B)
+int pb(elem **stacka, elem **stackb)
+{
+    if (*stacka == NULL)
+        return 0;
+    elem *topa;
+	
+	topa = *stacka;
+    *stacka = topa->next;
+    if (*stacka)
+        (*stacka)->prev = NULL;
+    topa->next = *stackb;
+    if (*stackb)
+        (*stackb)->prev = topa;
+    *stackb = topa;
+    topa->prev = NULL;
+    return 1;
+}
+//ROTATE A (SHIFTS UP ALL BY 1, FIRST BECOMES LAST)
+int ra(elem**stacka)
+{
+	if(*stacka == NULL || (*stacka)->next == NULL)
+		return 0;
+	elem *last;
+	elem *first;
+	
+	last = *stacka;
+	while(last->next != NULL)
+		last = last->next;
+	first = *stacka;
+	first->prev = last;
+	(first->next)->prev = NULL; 
+	*stacka = first->next;
+	last->next = first;
+	first->next = NULL;
+	return(1);
+}
+//ROTATE B (SHIFTS UP ALL BY 1, FIRST BECOMES LAST)
+int rb(elem**stackb)
+{
+	if(*stackb == NULL || (*stackb)->next == NULL)
+		return 0;
+	elem *last;
+	elem *first;
+	
+	last = *stackb;
+	while(last->next != NULL)
+		last = last->next;
+	first = *stackb;
+	first->prev = last;
+	(first->next)->prev = NULL; 
+	*stackb = first->next;
+	last->next = first;
+	first->next = NULL;
+	return(1);
+}
+int rr(elem *stacka, elem *stackb)
+{
+	if(ra(&stacka))
+		if(rb(&stackb))
+			return (1);
+	return (0);
+}
+//Reverse rotate A (SHIFTS DOWN ALL BY 1, LAST BECOMES FIRST)
+int rra(elem **stacka)
+{
+	if(stacka == NULL || *stacka == NULL || (*stacka)->next == NULL)
+		return (0);
+		
+	elem *last;
+	elem *secondlast;
+	elem *first;
+	
+	last = *stacka;
+	while(last->next != NULL)
+		last = last->next;
+	secondlast = last->prev;
+	secondlast->next = NULL;
+	last->prev = NULL;
+	first = *stacka;
+	last -> next = first;
+	first -> prev = last;
+	*stacka = last;
+	return(1);
+}
+//Reverse rotate B (SHIFTS DOWN ALL BY 1, LAST BECOMES FIRST)
+int rrb(elem **stackb)
+{
+	if(stackb == NULL || *stackb == NULL || (*stackb)->next == NULL)
+		return (0);
+		
+	elem *last;
+	elem *secondlast;
+	elem *first;
+	
+	last = *stackb;
+	while(last->next != NULL)
+		last = last->next;
+	secondlast = last->prev;
+	secondlast->next = NULL;
+	last->prev = NULL;
+	first = *stackb;
+	last -> next = first;
+	first -> prev = last;
+	*stackb = last;
+	return(1);
+}
+//Reverse rotate both
+int rrr(elem **stacka, elem **stackb)
+{
+	if(rra(stacka) && rrb(stackb))
+		return (1);
+	return(0);
+}
 int main(void)
 {
 	elem *stacka;
@@ -138,8 +251,11 @@ int main(void)
 	addElemOnTop(&stacka, 14);
 	addElemOnTop(&stackb,5);
 	addElemOnTop(&stackb,8);
-	sa(&stacka);
-	pa(&stacka,&stackb);
+	addElemOnTop(&stackb,3);
+	addElemOnTop(&stackb,10);
+	printStack(stacka);
+	printStack(stackb);
+	rrr(&stacka,&stackb);
 	printStack(stacka);
 	printStack(stackb);
 	return (0);
